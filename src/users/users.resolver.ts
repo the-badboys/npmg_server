35fb2fa2548e383ea3e0862414d75f51dcafa6b7
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-02-22 20:46:39 +0200

    setup grapqhl

---
+++ src/users/users.resolver.ts
@@ -0,0 +1,38 @@
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { GetUserArgs } from './dto/args/getUser.args';
+import { GetUsersArgs } from './dto/args/getUsers.args';
+import { CreateUserInput } from './dto/input/create-user.input';
+import { DeleteUserInput } from './dto/input/delete-user.input';
+import { UpdateUserInput } from './dto/input/update-user-input';
+import { User } from './models/user';
+import { UsersService } from './users.service';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly userService: UsersService) {}
+
+  @Query(() => User, { name: 'user', nullable: true })
+  getUser(@Args() getUserArgs: GetUserArgs): User {
+    return this.userService.getUser(getUserArgs);
+  }
+
+  @Query(() => [User], { name: 'users', nullable: 'items' })
+  getUsers(@Args() getUsersArgs: GetUsersArgs): User[] {
+    return this.userService.getUsers(getUsersArgs);
+  }
+
+  @Mutation(() => User)
+  createUser(@Args('createUserData') createUserData: CreateUserInput): User {
+    return this.userService.createUser(createUserData);
+  }
+
+  @Mutation(() => User)
+  updateUser(@Args('updateUserData') updateUserData: UpdateUserInput): User {
+    return this.userService.updateUser(updateUserData);
+  }
+
+  @Mutation(() => User)
+  deleteUser(@Args('deleteUserData') deleteUserData: DeleteUserInput): User {
+    return this.userService.deleteUser(deleteUserData);
+  }
+}
