47cf2665a4b025e982b2d4e36e6d7b520228cd4b
didiermunezer38 <didiermunezer38@gmail.com>
2021-02-26 20:25:01 +0200

    done with attendance

--- src/attendance/attendance.resolver.ts
+++ src/attendance/attendance.resolver.ts
@@ -1,4 +1,69 @@
-import { Resolver } from '@nestjs/graphql';
+import {
+    Resolver,
+    Query,
+    Args,
+    Mutation,
+    Context,
+  } from '@nestjs/graphql';
+  import { Inject } from '@nestjs/common';
+  import {NewAttendance} from './inputs/create.input'
+  import {UpdateAttendance} from './inputs/update.input'
+  import {Attendance} from './dto/attendance'
+  import { PrismaService } from 'src/prisma.service';
+  import {AuthenticationError, UserInputError} from 'apollo-server-express'
 
-@Resolver()
-export class AttendanceResolver {}
+@Resolver(Attendance)
+export class AttendanceResolver {
+    constructor(@Inject(PrismaService) private prismaService: PrismaService) {}
+    @Query(returns => [Attendance], { nullable: true, name: 'getAllAttendances' })
+    async allReports(@Context() ctx) {
+      return this.prismaService.attendance.findMany({});
+    }
+    @Query(returns => Attendance, { nullable: true, name: 'getAttendance' })
+    async attendance(@Args('id') id: string, @Context() ctx) {
+      return this.prismaService.attendance.findUnique({
+        where: { id },
+      });
+    }
+    @Query(returns => [Attendance], { nullable: true, name: 'getUserAttendance' })
+    async user_attendance(@Args('id') id: string, @Context() ctx) {
+      return this.prismaService.attendance.findMany({
+        where: { attendant: id },
+      });
+    }
+    @Query(returns => [Attendance], { nullable: true, name: 'getDayAttendance' })
+    async day_attendance(@Args('date') day: Date, @Context() ctx) {
+      return this.prismaService.attendance.findMany({
+        where: { date: day },
+      });
+    }
+    @Mutation(returns => Attendance)
+    async newAttendance(@Args('data') data: NewAttendance, @Context() ctx) {
+      const user = await this.prismaService.users.findUnique({
+        where: { id:data.attendant },
+      });
+      if(!user){
+        return new UserInputError("Ranger not found")
+      }
+      const adder = await this.prismaService.users.findUnique({
+        where: { id:data.added_by },
+      });
+      if(!adder){
+        return new UserInputError("Reporter not found")
+      }
+      return this.prismaService.attendance.create({
+        data: {
+          attendant: data.attendant,
+          added_by: data.added_by,
+          date: data.date,
+          isPresent: data.isPresent,
+        },
+      });
+    }
+    @Mutation(returns => Attendance, { nullable: true, name: 'deleteAttendance' })
+  async delete(@Args('id') id: string, @Context() ctx) {
+    return this.prismaService.attendance.delete({
+      where: { id },
+    });
+  }
+}
--- src/tasks/tasks.resolver.ts
+++ src/tasks/tasks.resolver.ts
@@ -1,4 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import {
+    Resolver,
+    Query,
+    Args,
+    Mutation,
+    Context,
+  } from '@nestjs/graphql';
+  import { Inject } from '@nestjs/common';
+  import {NewTask} from './inputs/create.input'
+  import {UpdateTask} from './inputs/update.input'
+  import {Task} from './dto/tasks'
+  import { PrismaService } from 'src/prisma.service';
+  import {AuthenticationError, UserInputError} from 'apollo-server-express'
 
-@Resolver()
+@Resolver(Task)
 export class TasksResolver {}
