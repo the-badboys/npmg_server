04b2e89786cee459d3d6d345b77dfd56465331a1
didiermunezer38 <didiermunezer38@gmail.com>
2021-02-26 20:41:30 +0200

    cool now killer error removed

--- src/tasks/tasks.resolver.ts
+++ src/tasks/tasks.resolver.ts
@@ -13,4 +13,52 @@ import {
   import {AuthenticationError, UserInputError} from 'apollo-server-express'
 
 @Resolver(Task)
-export class TasksResolver {}
+export class TasksResolver {
+    constructor(@Inject(PrismaService) private prismaService: PrismaService) {}
+    @Query(returns => [Task], { nullable: true, name: 'getAllTasks' })
+    async allReports(@Context() ctx) {
+      return this.prismaService.tasks.findMany({});
+    }
+    @Query(returns => Task, { nullable: true, name: 'getTask' })
+    async attendance(@Args('id') id: string, @Context() ctx) {
+      return this.prismaService.tasks.findUnique({
+        where: { id },
+      });
+    }
+    @Query(returns => [Task], { nullable: true, name: 'getGroupTasks' })
+    async user_attendance(@Args('id') id: string, @Context() ctx) {
+      return this.prismaService.tasks.findMany({
+        where: { group: id },
+      });
+    }
+    @Query(returns => [Task], { nullable: true, name: 'getDayTasks' })
+    async day_attendance(@Args('date') day: Date, @Context() ctx) {
+      return this.prismaService.tasks.findMany({
+        where: { date: day },
+      });
+    }
+    @Mutation(returns => Task)
+    async newAttendance(@Args('data') data: NewTask, @Context() ctx) {
+      const user = await this.prismaService.users.findUnique({
+        where: { id:data.added_by },
+      });
+      if(!user){
+        return new UserInputError("Leader not found")
+      }
+      return this.prismaService.tasks.create({
+        data: {
+          group: data.group,
+          added_by: data.added_by,
+          family: data.family,
+          date: data.date,
+          isCompleted: data.isCompleted,
+        },
+      });
+    }
+    @Mutation(returns => Task, { nullable: true, name: 'deleteAttendance' })
+  async delete(@Args('id') id: string, @Context() ctx) {
+    return this.prismaService.tasks.delete({
+      where: { id },
+    });
+  }
+}
