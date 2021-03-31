810450a85a66d27529d2716de048d5504b9a4cc6
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-31 18:11:12 +0200

    feat: fix

--- src/groups/groups.ts
+++ src/groups/groups.ts
@@ -5,26 +5,20 @@ import { User } from 'src/users/user';
 @ObjectType()
 export class groups {
   @Field()
-  @IsNotEmpty()
   id: string;
 
   @Field()
-  @IsNotEmpty()
   leaderId: string;
 
   @Field()
-  @IsNotEmpty()
   name: string;
 
   @Field()
-  @IsNotEmpty()
   description: string;
 
   @Field()
-  @IsNotEmpty()
   created_at: Date;
 
   @Field()
-  @IsNotEmpty()
   leader: User;
 }
