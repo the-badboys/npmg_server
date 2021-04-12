8bc355a9af22e1559135b6438f081a83989ad4cc
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-04-12 21:40:31 +0200

    feat: working on grobal error exceptions

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './utils/jwtSetup';
 import { NotificationModule } from './notification/notification.module';
 import { GroupsModule } from './groups/groups.module';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
 
 @Module({
   imports: [
@@ -24,57 +25,60 @@ import { GroupsModule } from './groups/groups.module';
       playground: true,
       introspection: true,
       installSubscriptionHandlers: true,
-      //TODO: (verite:) Error formatting cleanup
-      // formatError: (error: GraphQLError) => {
-      //   if (error.message === 'VALIDATION_ERROR') {
-      //     const extensions = {
-      //       code: 'VALIDATION_ERROR',
-      //       errors: [],
-      //     };
+      formatError: (error: GraphQLError) => {
+        if (error.message === 'VALIDATION_ERROR') {
+          const extensions = {
+            code: 'VALIDATION_ERROR',
+            errors: [],
+          };
 
-      //     Object.keys(error.extensions.invalidArgs).forEach(key => {
-      //       const constraints = [];
-      //       Object.keys(error.extensions.invalidArgs[key].constraints).forEach(
-      //         _key => {
-      //           constraints.push(
-      //             error.extensions.invalidArgs[key].constraints[_key],
-      //           );
-      //         },
-      //       );
+          Object.keys(error.extensions.invalidArgs).forEach(key => {
+            const constraints = [];
+            Object.keys(error.extensions.invalidArgs[key].constraints).forEach(
+              _key => {
+                constraints.push(
+                  error.extensions.invalidArgs[key].constraints[_key],
+                );
+              },
+            );
 
-      //       extensions.errors.push({
-      //         field: error.extensions.invalidArgs[key].property,
-      //         errors: constraints,
-      //       });
-      //     });
+            extensions.errors.push({
+              field: error.extensions.invalidArgs[key].property,
+              errors: constraints,
+            });
+          });
 
-      //     const graphQLFormattedError: GraphQLFormattedError = {
-      //       message: 'VALIDATION_ERROR',
-      //       extensions: extensions,
-      //     };
+          const graphQLFormattedError: GraphQLFormattedError = {
+            message: 'VALIDATION_ERROR',
+            extensions: extensions,
+          };
 
-      //     return graphQLFormattedError;
-      //   } else {
-      //     if (error.extensions.exception.name === 'JsonWebTokenError') {
-      //       const graphQLFormattedError = {
-      //         status: 404,
-      //         message: 'Invalid token',
-      //         error: 'Unauthorized',
-      //       };
-      //       return graphQLFormattedError;
-      //     } else if (error.extensions.exception.code === 'P2002') {
-      //       const field = error.extensions.exception.meta.target[0];
-      //       const graphQLFormattedError = {
-      //         status: 404,
-      //         message: 'Invalid Data',
-      //         error: `${field} Already exists`,
-      //       };
-      //       return graphQLFormattedError;
-      //     } else {
-      //       return error.extensions?.exception?.response || error.message;
-      //     }
-      //   }
-      // },
+          return graphQLFormattedError;
+        } else {
+          if (error.extensions.exception.name === 'JsonWebTokenError') {
+            const graphQLFormattedError = {
+              status: 404,
+              message: 'Invalid token',
+              error: 'Unauthorized',
+            };
+            return graphQLFormattedError;
+          } else if (error.extensions.exception.code === 'P2002') {
+            const field = error.extensions.exception.meta.target[0];
+            const graphQLFormattedError = {
+              status: 404,
+              message: 'Invalid Data',
+              error: `${field} Already exists`,
+            };
+            return graphQLFormattedError;
+          } else {
+            return {
+              // error: error.extensions?.exception,
+              status: error.extensions?.exception.status,
+              message: error.extensions?.exception?.response,
+            };
+          }
+        }
+      },
     }),
     UsersModule,
     NpmgModule,
@@ -91,7 +95,3 @@ import { GroupsModule } from './groups/groups.module';
   providers: [],
 })
 export class AppModule {}
-
-//TODO: (Verite) I shall use this one to update error handling mechanism we are using
-// formatError: (error: GraphQLError) =>
-//   error.extensions?.exception?.response || error.message,
